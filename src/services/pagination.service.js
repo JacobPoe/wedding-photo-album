9b84ec2c8d2931eeaf94cb2d65ecc530d1db93a2
Jake Poe <jacobpoe@gmail.com>
2025-12-14 15:56:36 -0500

    Apply function name change to use in pagination.service

--- src/services/pagination.service.js
+++ src/services/pagination.service.js
@@ -1,10 +1,10 @@
 const paginate = (props) => {
     switch (props.direction) {
         case 'next':
-            props.dispatcher(props.offset + 1)
+            props.updateOffset(props.offset + 1)
             return;
         case 'prev':
-            props.dispatcher(props.offset - 1)
+            props.updateOffset(props.offset - 1)
             return;
         default:
             return;
