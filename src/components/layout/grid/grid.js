74e1a61732f49be5f7cb7d61d38244bb692c22e1
Jake Poe <jacobpoe@gmail.com>
2025-12-14 13:17:32 -0500

    Add controls to <Grid />, update batchSize in state on dropdown update

--- src/components/layout/grid/grid.js
+++ src/components/layout/grid/grid.js
@@ -3,11 +3,16 @@ import { connect } from "react-redux";
 
 import { buildTileMeta, loadAssets } from "../../../services/asset.service";
 import { setAssets } from "../../../state/actions/set-assets";
+import { setBatchSize } from "../../../state/actions/set-batch-size";
 
 import Tile from "../tile/tile";
+import Select from "../../controls/select/select";
+
 import './grid.css';
+import Button from "../../controls/button/button";
 
 const Grid = (props) => {
+    const gridSizeOptions = [25, 50, 100, 200, 'all']
     const [tiles, setTiles] = useState([]);
 
     const loadTiles = async () => {
@@ -19,6 +24,16 @@ const Grid = (props) => {
                 })
             )
         })
+    }
+
+    const updateGridSize = (val) => {
+        let batchSize;
+        if (val === 'all') {
+            batchSize = -1;
+        } else {
+            batchSize = val;
+        }
+        props.dispatch(setBatchSize(batchSize));
     }
 
     useEffect(() => {
@@ -44,21 +59,49 @@ const Grid = (props) => {
 
     return (
         <div className="grid" id="grid">
-            {
-                tiles.map((image, key) =>
-                    <Tile
-                        id={`tile_${key}`}
-                        name={image.name}
-                        url={image.url}
-                        key={key}
-                        tabIndex={key + 1}
-                    />)
-            }
+            <div className={`row grid__row grid__controls`}>
+                <Select
+                    id="gridsize"
+                    initialValue={props.batchSize}
+                    opts={gridSizeOptions}
+                    callbackFn={(e) => updateGridSize(e)}
+                />
+                <div className={`grid__controls--nav`}>
+                    <Button
+                        id="prev"
+                        label="Previous"
+                        type="nav"
+                        className={['button__nav', 'button__nav-prev', 'btn--secondary']}
+                        text={'<'}
+                    />
+                    <Button
+                        id="next"
+                        label="Next"
+                        type="nav"
+                        className={['button__nav', 'button__nav-next', 'btn--secondary']}
+                        text={'>'}
+                    />
+                </div>
+
+            </div>
+            <div className="grid__tiles">
+                {
+                    tiles.map((image, key) =>
+                        <Tile
+                            id={`tile_${key}`}
+                            name={image.name}
+                            url={image.url}
+                            key={key}
+                            tabIndex={key + 1}
+                        />)
+                }
+            </div>
         </div>
     )
 }
 
 export default connect((state) => ({
     activeTab: state.navigation.activeTab,
+    batchSize: state.navigation.batchSize,
     directories: state.album.directories
 }))(Grid);
