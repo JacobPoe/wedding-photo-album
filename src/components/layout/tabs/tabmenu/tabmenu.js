94d3e69035c6d957c3c5ac5def22d536d4c04633
Jake Poe <jacobpoe@gmail.com>
2025-12-23 12:56:04 -0500

    Convert tabmenu's <section> wrapper to <div>

--- src/components/layout/tabs/tabmenu/tabmenu.js
+++ src/components/layout/tabs/tabmenu/tabmenu.js
@@ -4,12 +4,12 @@ import Tab from "../tab/tab";
 
 const TabMenu = (props) => {
     return (
-        <section>
+        <div>
             {props.tabs.map((tab, index) => {
                 return (<Tab category={tab} index={index} key={index}/>);
             })}
             <hr/>
-        </section>
+        </div>
     )
 }
 
