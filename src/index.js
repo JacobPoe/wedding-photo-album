0a99d8c346b429948efd6d7155316b34b41657a6
Jake Poe <jakeapoe@gmail.com>
2026-02-06 22:59:48 -0500

    Load shared stylesheet from npm package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import './style.css';
-import Home from "./components/views/home/home";
-
 import React from "react";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
@@ -8,8 +5,10 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { album } from "jake-compoenents/dist/state/reducers/album";
 import { navigation } from "jake-compoenents/dist/state/reducers/navigation";
-
 import logger from "jake-compoenents/dist/state/middleware/logger";
+
+import "jake-compoenents/dist/style.css";
+import Home from "./components/views/home/home";
 
 const store = configureStore({
     reducer: { album: album, navigation: navigation },
