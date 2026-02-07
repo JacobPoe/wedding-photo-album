f96f14a73d69827e2b62620d2274690c14edc94a
Jake Poe <jakeapoe@gmail.com>
2026-02-06 22:55:38 -0500

    Remove unused state code that lives in shared npm package

--- src/state/actions/set-active-image.js
+++
@@ -1,8 +0,0 @@
-import { SET_ACTIVE_IMAGE } from "../reducers/album";
-
-export function setActiveImage(activeImage) {
-    return {
-        type: SET_ACTIVE_IMAGE,
-        activeImage
-    }
-}
--- src/state/actions/set-directories.js
+++
@@ -1,8 +0,0 @@
-import { SET_DIRECTORIES } from "../reducers/album";
-
-export function setDirectories(directories) {
-    return {
-        type: SET_DIRECTORIES,
-        directories
-    }
-}
